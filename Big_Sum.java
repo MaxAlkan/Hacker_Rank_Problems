aa95d54997319c6cb305d69f0a0e17c8cdf5ecad
MaxAlkan <max.b.garcia0@gmail.com>
2015-09-24 16:50:08 -0700

    Create Big_Sum.java

---
+++ Big_Sum.java
@@ -0,0 +1,39 @@
+/**
+ *
+ * @author Max Alkan
+ */
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Big_Sum {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Takes user input and converts into a character array
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        String input = sc.nextLine();
+        String[] a = input.split(" ");
+        
+        BigInteger[] storage = new BigInteger[n];
+        BigInteger sum = new BigInteger("0");
+        
+        //Converts and Stores Big Integers
+        for(int i = 0; i < n; i++)
+            storage[i] = new BigInteger(a[i]);
+        
+        //Sums the BigIntegers in storage
+        for(int j = 0; j < n; j++) 
+            sum = sum.add(storage[j]);
+
+        //Outputs the sum of values in storage as a string
+        System.out.println(sum);
+    }   
+}
